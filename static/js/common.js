9ecc407a877eefef62ad282e2b70dfbab769b732
杨秀会 <wb-yangxiuhui>
2015-01-22 22:27:55 +0800

    js

--- static/js/common.js
+++ static/js/common.js
@@ -967,10 +967,40 @@ JS Document
 	}
 	// 这个方法是以前支付宝海外转运项目里拿下来的代码。一行一行解读一下代码吧...
 	fnTsh.milliFormat = function(s, decimal){
+		
+		// 如果是input输入框取光标位置
+		this.milliFormat.selectionStart = function( input ){
+			 
+	        var cursurPosition = -1;
+
+	        if(input.selectionStart != undefined){
+	            cursurPosition = input.selectionStart;
+	        }else{
+	            var range = document.selection.createRange();
+	            range.moveStart("character", -input.value.length);
+	            cursurPosition= range.text.length;
+	        }
+
+	        return cursurPosition;
+		};
+		this.milliFormat.setSelectionRange = function( input, pos ){
+				
+				var cursurPosition = -1;
+
+                if(input.selectionStart != undefined){
+                    input.setSelectionRange(pos,pos);
+                }
+                else{
+                    var range = input.createTextRange();
+                    range.move("character", pos);
+                    range.select();
+                }
+		};
+		
 		// 首先转string类型，保证下面的replace,test等方法执行
         s = s + '';
         // 发现input的keyup事件时，有bug需要过虑逗号
-        s = s.replace(/,/, '');
+        s = s.replace(/,/g, '');
         // s字符串只能出现数字与.这两种字符，否则return 无效的value中止下面的操作
         if(/[^0-9\.]/.test(s)) {
             //return "invalid value";
