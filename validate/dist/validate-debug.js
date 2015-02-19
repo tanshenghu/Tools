d12d4f825778268a1e18f2a8386ded5eb3356fcf
谭生虎 <tanshenghu@163.com>
2015-02-19 19:00:24 +0800

    update

--- validate/dist/validate-debug.js
+++ validate/dist/validate-debug.js
@@ -3,6 +3,7 @@ define("tanshenghu/validate/1.0.0/validate-debug", [ "$-debug", "./validate.exte
         // 验证 初始化，最终返回布尔值 true/false
         init: function(form) {
             this.form = $(form);
+            this.rules.This = this.form;
             this.blur();
             return this.verify() && this.form.find(".formTip").length === 0;
         },
@@ -51,14 +52,21 @@ define("tanshenghu/validate/1.0.0/validate-debug", [ "$-debug", "./validate.exte
                 });
                 return result;
             } else {
-                $.tsh.log(rls + " erroneous rules");
+                window.console && console.log(rls + " erroneous rules");
             }
         },
         // 校验 规则 只列部分。为减小文件体积将规则写进扩展文件里面
         rules: {
             required: function(ele) {
-                var value = $(ele).val().trim();
-                return value.length === 0 ? false : true;
+                var ele = $(ele), type = ele.attr("type").toLowerCase(), This = this.This, lenVal;
+                if (type === "radio" || type === "checkbox") {
+                    var name = ele.attr("name");
+                    var checked = This.form.find('[name="' + name + '"]:checked');
+                    lenVal = checked.length === 0 ? false : true;
+                } else {
+                    lenVal = ele.val().trim().length === 0 ? false : true;
+                }
+                return lenVal;
             },
             email: function(ele) {
                 var result = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i.test($(ele).val());
